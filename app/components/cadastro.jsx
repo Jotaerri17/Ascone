2adfd13c628de3ed779fcb82dcd4d504c4d43638
Exercícios.py2 <joaorafaelsantos17@gmail.com>
2025-08-08 18:41:34 -0300

    Add Cadastro component for contact registration and display

---
+++ app/components/cadastro.jsx
@@ -0,0 +1,73 @@
+'use client'
+import { useState } from 'react';
+import { toast } from "react-hot-toast"
+
+
+export default function Cadastro() {
+    const [nome, setNome] = useState('');
+    const [idade, setIdade] = useState('');
+    const [email, setEmail] = useState('');
+    const [contatos, setContatos] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        try {
+            const novoContato = {
+                nome: nome,
+                idade: idade,
+                email: email,
+            };
+
+            setContatos((contatosCriados) => [...contatosCriados, novoContato]);
+
+            setNome('');
+            setEmail('');
+            setIdade('');
+
+            toast.success("Contato adicionado com sucesso!");
+        } catch (error) {
+            toast.error("Erro.");
+        }
+    };
+
+    return (
+        <div className="w-full h-screen bg-white px-80"> 
+            
+            <div className='mt-20  flex justify-around w-full py-4'>
+                <div className='w-96'>
+                    <h1 className='text-2xl px-2 py-4 rounded-md text-center'>Cadastro de Contato</h1>
+                    <div>
+                        <form >
+                            <div className='flex flex-col gap-1'>
+                                <label>Nome:</label>
+                                <input type="text" className='border-0.5 border-zinc-600  rounded-md py-2 focus:ring-2 px-3 ring-2 focus:ring-blue-600 outline-none focus:border-none' value={nome} onChange={(e) => setNome(e.target.value)} placeholder='Digite seu nome...'/>
+                            </div>
+                            <div className=' flex flex-col gap-1 mt-1 '>
+                                <label>Idade:</label>
+                                <input type='text' className='border-0.5 border-zinc-600  rounded-md py-2 focus:ring-2 px-3 ring-2 focus:ring-blue-600 outline-none focus:border-none' value={idade} onChange={(e) => setIdade(e.target.value)} placeholder='Digite sua idade...' />
+                            </div>
+                            <div className=' flex flex-col gap-1 mt-1 '>
+                                <label>Email:</label>
+                                <input type='text' className='border-0.5 border-zinc-600  rounded-md py-2 focus:ring-2 px-3 ring-2 focus:ring-blue-600 outline-none focus:border-none' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Digite seu email...' />
+                            </div>
+                            <button type='submit' onClick={handleSubmit} className='rounded-sm px-4 mt-4 py-2 hover:bg-cyan-600 duration-150 transition-all  bg-cyan-700 text-white font-semibold text-sm border-1  w-full cursor-pointer'>Enviar Contato</button>
+                        </form>
+                    </div>
+                </div>
+                <div>
+                    <h1 className='text-2xl px-2 py-4 rounded-md text-center'>Lista de contatos</h1>
+                    <div>
+                        {contatos.map((item) => (
+                            <div key={item.nome} className='mt-2 p-4 bg-gray-100 rounded-md shadow-sm'>
+                                <h1 className='font-bold'>nome: {item.nome}</h1>
+                                <p>Idade: {item.idade}</p>
+                                <p>Email: {item.email}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,106 +1,35 @@
 "use client"
 import Button from './components/button';
+import Cadastro from './components/cadastro';
 import { useState } from 'react';
 import { toast } from "react-hot-toast"
 
+
 export default function Home() {
 
-
-    const [nome, setNome] = useState('');
-    const [idade, setIdade] = useState('');
-    const [email, setEmail] = useState('');
-
-
-    const [contatos, setContatos] = useState([])
-
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-
-      
-        try {
-          const novoContato = {
-            nome: nome,
-            idade: idade,
-            email: email,
-          }
-
-          setContatos((contatosCriados) => [...contatosCriados, novoContato])
-
-          setNome('');
-          setEmail('');
-          setIdade('');
-
-          toast.success("Contato adicionado com sucesso!");
-
-        } catch (error) {
-
-          toast.error("Erro.")
-        } 
-    }
-
-  return (
-    <div className="w-full h-screen bg-white px-80"> 
-      
-      <Button />
-      
-      
-
-
-
-    <div className='mt-20  flex justify-around w-full py-4'>
-
-    <div className='w-96'>
-        <h1 className='text-2xl px-2 py-4 rounded-md text-center'>Cadastro de Contato</h1>
+  return(
+    <>
+      <header>
+             
         
-        <div>
-            <form >
-                <div className='flex flex-col gap-1'>
-                    <label>Nome:</label>
-                    <input type="text" className='border-0.5 border-zinc-600  rounded-md py-2 focus:ring-2 px-3 ring-2 focus:ring-blue-600 outline-none focus:border-none' value={nome} onChange={(e) => setNome(e.target.value)} placeholder='Digite seu nome...'/>
-                </div>
-                <div className=' flex flex-col gap-1 mt-1 '>
-                  <label>Idade:</label>
-                  <input type='text' className='border-0.5 border-zinc-600  rounded-md py-2 focus:ring-2 px-3 ring-2 focus:ring-blue-600 outline-none focus:border-none' value={idade} onChange={(e) => setIdade(e.target.value)} placeholder='Digite sua idade...' />
-                </div>
-                <div className=' flex flex-col gap-1 mt-1 '>
-                  <label>Email:</label>
-                  <input type='text' className='border-0.5 border-zinc-600  rounded-md py-2 focus:ring-2 px-3 ring-2 focus:ring-blue-600 outline-none focus:border-none' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Digite seu email...' />
-                </div>
-                
-                <button type='submit' onClick={handleSubmit} className='rounded-sm px-4 mt-4 py-2 hover:bg-cyan-600 duration-150 transition-all  bg-cyan-700 text-white font-semibold text-sm border-1  w-full cursor-pointer'>Enviar Contato</button>
-            </form>
-        </div>
-    </div>
+        <nav className=' flex  justify-between items-center py-6 mx-10'>
+          
+          <h1 className='text-black text-3xl  font-extrabold'>Ascone</h1>
 
-     <div>
-
-      <h1 className='text-2xl px-2 py-4 rounded-md text-center'>Lista de contatos</h1>
-
-
-      <div>
-
-        {contatos.map((item) => (
-              <div key={item.nome} className='mt-2 p-4 bg-gray-100 rounded-md shadow-sm'>
-                <h1 className='font-bold'>nome: {item.nome}</h1>
-                <p>Idade: {item.idade}</p>
-                <p>Email: {item.email}</p>
-              </div>
-            ))}
-      </div>
-
-
-
-     </div>
-
-
-
-      </div>
-
-     
-
-      
-    </div>
-  );
+          <div className='flex space-x-44'>
+            <h1 className=' text-black text-md cursor-pointer font-semibold hover:border-b-1'> Home </h1>
+            <h1 className=' text-black text-md cursor-pointer font-semibold hover:border-b-1'> Company </h1>
+            <h1 className=' text-black text-md cursor-pointer font-semibold hover:border-b-1'> Help </h1>
+          </div>
+          
+          <div className='flex space-x-10 items-center'>
+              <h1 className=' text-md cursor-pointer font-semibold'> Login </h1>
+              <h1 className=' text-black text-sm cursor-pointer border-2  border-green-800 rounded-2xl px-4 py-2 font-semibold hover:bg-green-900 hover:text-white transition-all duration-150'> Sign Up  </h1>
+          </div>
+          
+        </nav>
+      </header>  
+    </>
+  )
 }
 
