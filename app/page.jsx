337196214e654e95a4019e95c13e554e238adb4b
Jotaerri17 <joaorafaelsantos17@gmail.com>
2025-08-28 12:02:52 -0300

    Add lucide-react icons, update layout, and include image for enhanced UI

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,9 @@
 import Button from './components/button';
 import Cadastro from './components/cadastro';
 import { useState } from 'react';
-import { toast } from "react-hot-toast"
+import { toast } from "react-hot-toast";
+import { CreditCard, CircleDollarSign, Landmark, DollarSign, CircleUser } from "lucide-react";
+import Image from "next/image";
 
 
 export default function Home() {
@@ -24,7 +26,8 @@ export default function Home() {
           
           <div className='flex space-x-10 items-center'>
               <h1 className=' text-md cursor-pointer font-semibold'> Login </h1>
-              <h1 className=' text-white text-sm cursor-pointer border-2 border-green-900 bg-green-900 rounded-2xl px-4 py-2 font-semibold hover:bg-green-700 hover:border-green-700 duration-150'> Sign Up  </h1>
+              <h1 className='  text-white text-sm cursor-pointer border-2 border-green-900 bg-green-900 rounded-2xl px-4 py-2 font-semibold hover:bg-green-700 hover:border-green-700 duration-150'> Sign Up  </h1>
+              <h1 className=''><CircleUser size={40} className='text-green-900'/></h1>
           </div>
           
         </nav>
@@ -35,7 +38,7 @@ export default function Home() {
     <div className="flex flex-col md:flex-row min-h-screen bg-gradient-to-r from-gray-50 to-gray-300 p-8">
       
       
-      <div className="flex-1 flex flex-col justify-center p-8  ">
+      <div className="flex-1 flex flex-col justify-center p-10  ">
         <h1 className="text-6xl font-semibold italic">Mude a forma 
           como você usa
            seu dinheiro</h1>
@@ -50,8 +53,13 @@ export default function Home() {
       
       <div className="flex-1 p-4 flex items-center justify-center">
        
-        <div className="bg-white border border-gray-300 rounded-2xl w-full max-w-sm h-96 flex items-center justify-center text-gray-400">
-          (Img)
+        <div className="  relative bg-gradient-to-r from-gray-100 to-gray-200 border-gray-300 rounded-2xl w-full  h-96 flex items-center justify-center text-gray-400">
+          <Image
+            src="/Asconeimg.png"
+            alt="Descrição da imagem"
+            fill   // faz a imagem ocupar todo o espaço da div
+            className="object-contain rounded-lg"
+          />
         </div>
       </div>
     </div>  
@@ -61,14 +69,14 @@ export default function Home() {
         
       <div className='flex flex-col  bg-gradient-to-r from-gray-50 to-gray-300 h-[400px] w-full'>
 
-        <h1 className='flex  justify-center font-bold text-3xl'>Vantagens da Ascone</h1>
+        <h1 className='flex   items-center gap-4 justify-center font-bold text-3xl'><DollarSign size={35} strokeWidth={3} className='text-green-900' />Vantagens da Ascone<DollarSign size={35} strokeWidth={3}  className='text-green-900' /></h1>
 
         <div className='flex  justify-around  h-[300px] '>
 
           <div className='flex flex-col mt-12 w-sm items-center border-2 rounded-3xl  bg-gradient-to-br from-gray-100 to-gray-500'>
 
             
-            <h1 className='font-bold mt-4'>Vantagens do Cartão</h1>
+            <h1 className=' flex   items-center gap-2 font-bold mt-4'><CreditCard  size={20} strokeWidth={3} className='text-green-900' />Vantagens do Cartão</h1>
             <ul className=' list-disc flex flex-col mt-8  font-semibold gap-4'>
               
               <li>Cartão sem anuidade para sempre</li>
@@ -80,18 +88,18 @@ export default function Home() {
 
 
           <div className='flex flex-col mt-12 items-center w-sm  border-2 rounded-2xl  bg-gradient-to-br from-gray-100 to-gray-500'>
-            <h1 className='font-bold mt-4'>Vantagens Financeiras</h1>
+            <h1 className='flex   items-center gap-2 font-bold mt-4'><CircleDollarSign  size={20}  strokeWidth={3} className='text-green-900' />Vantagens Financeiras</h1>
             <ul className=' list-disc flex flex-col mt-8  font-semibold gap-4'>
               <li>Rendimento automático do seu dinheiro</li>
               <li>Transferências e PIX gratuitos</li>
-              <li>Empréstimos com taxas justas e transparentes</li>
+              <li>Empréstimos com taxas justas </li>
             </ul>
           </div>
 
 
 
           <div className='flex flex-col mt-12 items-center w-sm border-2 rounded-2xl  bg-gradient-to-br from-gray-100 to-gray-500'>
-            <h1 className='font-bold mt-4'>Estilo de Vida</h1>
+            <h1 className=' flex items-center gap-2 font-bold mt-4'><Landmark  size={20}  strokeWidth={3} className='text-green-900' />Estilo de Vida</h1>
             <ul className=' list-disc flex flex-col mt-8  font-semibold gap-4'>
               <li>Seu banco em qualquer lugar do mundo</li>
               <li>Pagamentos rápidos por aproximação</li>
